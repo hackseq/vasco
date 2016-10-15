a508398154df93d9d2367d5abef914a7bd5ec297
OganM <ogan.mancarcii@gmail.com>
2016-10-15 10:26:39 -0700

    shiny skeleton

---
+++ server.R
@@ -0,0 +1,30 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+library(readr)
+library(dplyr)
+library(Matrix)
+library(plotly)
+library(magrittr)
+
+#plot_ly(pbmc3k, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id)
+
+source('helpers.R')
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+    output$tSNEPlot <- renderPlotly({
+        # size of the bins depend on the input 'bins'
+        plot_ly(pbmc3k, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id)
+
+    })
+})
---
+++ ui.R
@@ -0,0 +1,26 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Single cell vis"),
+  tabsetPanel(
+    tabPanel('tSNE',
+             plotlyOutput('tSNEPlot')
+    ),
+    tabPanel('Next big thing')
+  )
+
+)
+
+)
