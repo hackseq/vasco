ea74c9c0960d699680c1ac0a0ee811c884b91146
OganM <ogan.mancarcii@gmail.com>
2016-10-15 10:43:43 -0700

    name changed to tSNE to make it sound dataset independent

--- helpers.R
+++ helpers.R
@@ -7,5 +7,5 @@ library(magrittr)
 barcodes = read_tsv('Data/redstone_1_barcodes.tsv', col_names = 'Barcode')
 
 genes = read_tsv('Data/redstone_1_genes.tsv',col_names = c('ID','Symbol'))
-pbmc3k = read_tsv('Data/redstone_pbmc3k_tdf', skip= 1, col_name = c('barcode','tSNE_1',	'tSNE_2','id' ))
+tsne = read_tsv('Data/redstone_pbmc3k_tdf', skip= 1, col_name = c('barcode','tSNE_1',	'tSNE_2','id' ))
 matrix = readMM('Data/redstone_1_matrix.mtx')
--- server.R
+++ server.R
@@ -15,7 +15,6 @@ library(Matrix)
 library(plotly)
 library(magrittr)
 
-#plot_ly(pbmc3k, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id)
 
 source('helpers.R')
 
@@ -24,7 +23,7 @@ source('helpers.R')
 shinyServer(function(input, output) {
     output$tSNEPlot <- renderPlotly({
         # size of the bins depend on the input 'bins'
-        plot_ly(pbmc3k, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id)
+        plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id)
 
     })
 })
