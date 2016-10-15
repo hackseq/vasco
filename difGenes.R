ee19b1a0300b89ed0a2004e1bf5c63bd0d6ac1d2
OganM <ogan.mancarcii@gmail.com>
2016-10-15 15:59:02 -0700

    difGenes fix

--- difGenes.R
+++ difGenes.R
@@ -3,15 +3,15 @@ difGenes = function(group1,group2, geneCount = 10){
   group2 = expression[,group2]
 
   group1Median = group1 %>% apply(1,mean)
-  group2Median = group2 %>% apply(1,mean)
+  group1Median = group2 %>% apply(1,mean)
   
   fChange = group1Median - group2Median
   
   difGeneOrder = order(fChange, decreasing = TRUE)
   data.frame(Gene.Symbol = genes$Symbol[difGeneOrder][1:geneCount],
              difference = fChange[difGeneOrder][1:geneCount],
-             group1Expression = group1[difGeneOrder][1:geneCount],
-             group2Expression = group2[difGeneOrder][1:geneCount])
+             group1Expression = group1Median[difGeneOrder][1:geneCount],
+             group2Expression = group1Median[difGeneOrder][1:geneCount])
 
   
 }
