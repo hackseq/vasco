2647ef2fee9980792c352f63a24789c4a71f7d45
OganM <ogan.mancarcii@gmail.com>
2016-10-15 10:22:15 -0700

    reading data

---
+++ helpers.R
@@ -0,0 +1,11 @@
+library(readr)
+library(dplyr)
+library(Matrix)
+library(plotly)
+library(magrittr)
+
+barcodes = read_tsv('Data/redstone_1_barcodes.tsv', col_names = 'Barcode')
+
+genes = read_tsv('Data/redstone_1_genes.tsv',col_names = c('ID','Symbol'))
+pbmc3k = read_tsv('Data/redstone_pbmc3k_tdf', skip= 1, col_name = c('barcode','tSNE_1',	'tSNE_2','id' ))
+matrix = readMM('Data/redstone_1_matrix.mtx')
