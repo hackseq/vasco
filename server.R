c0da4d795c5c4f4484e975787cb4e5cbadccc426
OganM <ogan.mancarcii@gmail.com>
2016-10-15 22:18:36 -0700

    goddamn plotly

--- server.R
+++ server.R
@@ -23,13 +23,14 @@ source('difGenes.R')
 shinyServer(function(input, output, session) {
   
   # debugging output, modify at will -----
-  output$debug <- renderPrint({ #differentiallyExpressed()
+  output$debug <- renderPrint({ 
+    #tsne[tsne$barcode %in% rownames(expression)[selected_vector1()],]
   })
   
   # main SNE plot ---------
   output$tSNEPlot <- renderPlotly({
     # size of the bins depend on the input 'bins'
-    plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode) %>%
+    plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source= 'hede') %>%
       layout(dragmode = "select")
     
   })
@@ -57,10 +58,12 @@ shinyServer(function(input, output, session) {
       layout(dragmode = "select")})
   
   # selection code and differential expression ------
-  selected_data <- reactive({event_data("plotly_selected")})
+  selected_data <- reactive({event_data("plotly_selected",source = 'hede')})
   
   selected_vector1 = reactive({
-    barcodes$Barcode %in% selected_data()$key
+    tsneSubset = tsne[tsne$tSNE_1 %in% selected_data()$x & tsne$tSNE_2 %in% selected_data()$y,]
+    print(table(tsneSubset$id))
+    barcodes$Barcode %in% tsneSubset$barcode
   })
   selected_vector2 = reactive({!selected_vector1()})
   
@@ -73,8 +76,33 @@ shinyServer(function(input, output, session) {
                group2 = selected_vector2())
     }
   })
-
-
+  
+  #observe({ write_tsv(differentiallyExpressed(), 'hede.tsv')})
+  
+  # dif genes heatmap
+  output$difHeatmap = renderPlotly({
+    print('rendering heatmap')
+    if(!is.null(differentiallyExpressed())){
+      orderedExpression  = expression[match(differentiallyExpressed()$Gene.Symbol,
+                                            genes$Symbol),]
+      print('expression ordered')
+      # the selection is isolated so it will wait for differential expression results
+      group1 = orderedExpression[,isolate(selected_vector1())]
+      group2 = orderedExpression[,isolate(selected_vector2())]
+      print(dim(group1))
+      print(dim(group2))
+      toPlot = cbind(group1,group2)
+      print('plotted')
+      plot_ly(z = toPlot[1:50,], x = '',
+              y = differentiallyExpressed()$Gene.Symbol[1:50] %>% as.character,
+              type = "heatmap")
+    } else{
+      print('or maybe not')
+      NULL
+    }
+  })
+  
+  
   
   # histogram of cells -----------
   output$countPerCluster <- renderPlotly({
