ce76e829eb99864515cae961bc66bd6f9aecd67d
OganM <ogan.mancarcii@gmail.com>
2016-10-16 11:04:44 -0700

    difGenes change name again

--- difGenes.R
+++ difGenes.R
@@ -9,8 +9,8 @@ difGenes = function(group1,group2){
   fChange = (group1Mean+0.1) / (group2Mean+0.1)
 
   difGeneOrder = order(fChange, decreasing = TRUE)
-  data.frame(Gene.Symbol = genes$Symbol[difGeneOrder],
-             difference = fChange[difGeneOrder],
+  data.frame("Gene Symbol" = genes$Symbol[difGeneOrder],
+             "Fold change" = fChange[difGeneOrder],
              "group1 Expression" = group1Mean[difGeneOrder],
              "group2 Expression" = group2Mean[difGeneOrder],
              check.names = FALSE)
