eef974243f52a9645ad88075e22682b3b5f3107a
OganM <ogan.mancarcii@gmail.com>
2016-10-17 14:13:53 -0700

    renaming clusters

--- server.R
+++ server.R
@@ -22,7 +22,8 @@ source('difGenes.R')
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   rValues = reactiveValues(selected_vector1 = NULL,
-                           selected_vector2 = NULL)
+                           selected_vector2 = NULL,
+                           tsne = tsne)
 
   #check if came from previous compare tab
   query_vals <- reactive({session$clientData$url_search
@@ -34,7 +35,7 @@ shinyServer(function(input, output, session) {
   # main SNE plot ---------
   output$tSNEPlot <- renderPlotly({
     # size of the bins depend on the input 'bins'
-    plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source= 'hede') %>%
+    plot_ly(rValues$tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source= 'hede') %>%
       layout(
         dragmode = "select",
         xaxis = list(title = tsne_xlab),
@@ -46,7 +47,7 @@ shinyServer(function(input, output, session) {
   #render initial seleciton plot
   output$tSNE_select_one <- renderPlotly({
     # size of the bins depend on the input 'bins'
-    plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source = "selection_plot_one") %>%
+    plot_ly(rValues$tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source = "selection_plot_one") %>%
       layout(
         dragmode = "select",
         xaxis = list(title = tsne_xlab),
@@ -89,7 +90,7 @@ shinyServer(function(input, output, session) {
   #render second selection plot when first population locked-in
   output$tSNE_select_two <- renderPlotly({
     input$pop_one_selected
-    isolate( plot_ly(tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source = "selection_plot_two") %>%
+    isolate( plot_ly(rValues$tsne, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~id, key = ~barcode, source = "selection_plot_two") %>%
       layout(
         dragmode = "select",
         xaxis = list(title = tsne_xlab),
@@ -123,12 +124,50 @@ shinyServer(function(input, output, session) {
      # layout(dragmode = "select")})
 
 
+  # code for setting the clusters -----
+  
+  observe({
+    if(input$selectDefinedGroupForRename){
+      show(id = 'whichGroupsForRename')
+    } else{
+      updateCheckboxGroupInput(session, inputId = 'whichGroupsForRename', choices = unique(rValues$tsne$id), selected = NULL)
+      hide(id = 'whichGroupsForRename')
+    }
+  })
+  
+  observe({
+    if(input$renamePoulationButton==1){
+      isolate({
+        if(!input$selectDefinedGroupForRename){
+          tsneSubset = rValues$tsne[rValues$tsne$tSNE_1 %in% selected_data()$x & rValues$tsne$tSNE_2 %in% selected_data()$y,]
+        } else{
+          tsneSubset = rValues$tsne[rValues$tsne$id %in% input$whichGroupsForRename,]
+        }
+        out = tsneSubset$barcode
+        
+        print('setting tsne ids')
+        
+        rValues$tsne[rValues$tsne$barcode %in% out,'id'] = input$newClusterName
+        
+        # update everything that uses old clusters
+        updateCheckboxGroupInput(session,
+                                 inputId = 'whichGroups',
+                                  label = 'Predefined clusters:',
+                                  choices = unique(rValues$tsne$id))
+        updateCheckboxGroupInput(session,
+                                 inputId = 'whichGroupsForRename',
+                                 label = 'Predefined clusters:',
+                                 choices = unique(rValues$tsne$id))
+    })
+  }
+})
+  
   # do I want to select defined groups?------
   observe({
     if(input$selectDefinedGroup){
       show(id = 'whichGroups')
     } else{
-      updateCheckboxGroupInput(session, inputId = 'whichGroups', choices = unique(tsne$id), selected = NULL)
+      updateCheckboxGroupInput(session, inputId = 'whichGroups', choices = unique(rValues$tsne$id), selected = NULL)
       hide(id = 'whichGroups')
     }
   })
@@ -141,9 +180,9 @@ shinyServer(function(input, output, session) {
                             label = 'Select predefined cluster(s) for group 2')
         print('group1 selection attempt')
         if(!input$selectDefinedGroup){
-          tsneSubset = tsne[tsne$tSNE_1 %in% selected_data()$x & tsne$tSNE_2 %in% selected_data()$y,]
+          tsneSubset = rValues$tsne[rValues$tsne$tSNE_1 %in% selected_data()$x & rValues$tsne$tSNE_2 %in% selected_data()$y,]
         } else{
-          tsneSubset = tsne[tsne$id %in% input$whichGroups,]
+          tsneSubset = rValues$tsne[rValues$tsne$id %in% input$whichGroups,]
         }
         rValues$selected_vector1 = barcodes$Barcode %in% tsneSubset$barcode
       })
@@ -157,14 +196,14 @@ shinyServer(function(input, output, session) {
       isolate({
         if(!input$selectDefinedGroup){
           if(!is.null(selected_data_two())){
-            tsneSubset = tsne[tsne$tSNE_1 %in% selected_data_two()$x & tsne$tSNE_2 %in% selected_data_two()$y,]
+            tsneSubset = rValues$tsne[rValues$tsne$tSNE_1 %in% selected_data_two()$x & rValues$tsne$tSNE_2 %in% selected_data_two()$y,]
             out = barcodes$Barcode %in% tsneSubset$barcode
           } else {
             # if nothing is selected, select the negative set based on tsne
-            out = barcodes$Barcode %in% tsne$barcode[!tsne$barcode %in% barcodes$Barcode[rValues$selected_vector1]]
+            out = barcodes$Barcode %in% rValues$tsne$barcode[!rValues$tsne$barcode %in% barcodes$Barcode[rValues$selected_vector1]]
           }
         } else{
-          tsneSubset = tsne[tsne$id %in% input$whichGroups,]
+          tsneSubset = rValues$tsne[rValues$tsne$id %in% input$whichGroups,]
           out = barcodes$Barcode %in% tsneSubset$barcode
         }
         rValues$selected_vector2 = out
@@ -175,9 +214,9 @@ shinyServer(function(input, output, session) {
   second_clicked_eds <-reactive({input$pop_two_selected
     barcodes_1 <- barcodes$Barcode[isolate({rValues$selected_vector1})]
     barcodes_2<- barcodes$Barcode[isolate({rValues$selected_vector2})]
-    g1 = tsne[ tsne$barcode %in% barcodes_1 & !(tsne$barcode %in% barcodes_2),]
-    g2 = tsne[tsne$barcode %in% barcodes_2  & !(tsne$barcode %in% barcodes_1),]
-    intersection = tsne[ tsne$barcode %in% barcodes_2 & tsne$barcode %in% barcodes_1 ,]
+    g1 = rValues$tsne[ rValues$tsne$barcode %in% barcodes_1 & !(rValues$tsne$barcode %in% barcodes_2),]
+    g2 = rValues$tsne[rValues$tsne$barcode %in% barcodes_2  & !(rValues$tsne$barcode %in% barcodes_1),]
+    intersection = rValues$tsne[ rValues$tsne$barcode %in% barcodes_2 & rValues$tsne$barcode %in% barcodes_1 ,]
     list(g1, g2, intersection)})
 
 
@@ -274,11 +313,11 @@ shinyServer(function(input, output, session) {
 
   # histogram of cells -----------
   output$cell_type_summary <- renderPlotly({
-    tsne_id <- table(tsne$id)
+    tsne_id <- table(rValues$tsne$id)
     categories<- dim(tsne_id)
     #make dummy array of all types of tsne clusters so that tables() returns an entry for each type
     dummy = data.frame(rep('AAAAAAAAAAAA', 8), rep(1.0, 8), rep(1.0, 8), names(tsne_id))
-    names(dummy) = names(tsne)
+    names(dummy) = names(rValues$tsne)
     groups <- second_clicked_eds()
     g1 <-  rbind(groups[[1]][c('barcode','tSNE_1',	'tSNE_2','id' )], dummy)
     g2 <-  rbind(groups[[2]][c('barcode','tSNE_1',	'tSNE_2','id' )], dummy)
@@ -344,7 +383,7 @@ shinyServer(function(input, output, session) {
       showticklabels = FALSE,
       showgrid = FALSE
     )
-    NumCells<-table(tsne$id)
+    NumCells<-table(rValues$tsne$id)
     NumCells<-as.data.frame(NumCells)
     plot_ly(NumCells, x=~Var1, y=~Freq, color=~Var1, type='bar') %>%
       layout(xaxis = ax,
