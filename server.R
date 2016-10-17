60cdd70e054be48038cedd4f85f33fc7cc5cba9e
OganM <ogan.mancarcii@gmail.com>
2016-10-16 18:13:41 -0700

    restructure selected genes

--- server.R
+++ server.R
@@ -21,6 +21,8 @@ source('difGenes.R')
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
+  rValues = reactiveValues(selected_vector1 = NULL,
+                           selected_vector2 = NULL)
 
   #check if came from previous compare tab
   query_vals <- reactive({session$clientData$url_search
@@ -91,7 +93,7 @@ shinyServer(function(input, output, session) {
     show(id= "div_select_two")
     show("pop_two_selected")
     hide(id="div_select_one")
-
+    
 
   })
 
@@ -103,7 +105,7 @@ shinyServer(function(input, output, session) {
     show('histPlot')
     show("reload")
   })
-
+  
   # Once group 1 and group 2 of cells are selected,
   # create 10 boxplots showing the gene expression distributions
   # of group 1 and group 2 for the top 10 up-regulated and
@@ -111,21 +113,21 @@ shinyServer(function(input, output, session) {
   output$histPlot <- renderPlotly({
     if ( !is.null(differentiallyExpressed()) ) {
       gene_cnt <- 10
-
-      nbr_group1 <- sum(selected_vector1())
-      nbr_group2 <- sum(selected_vector2())
+      
+      nbr_group1 <- sum(rValues$selected_vector1)
+      nbr_group2 <- sum(rValues$selected_vector2)
       nbr_barcodes <- nbr_group1 + nbr_group2
-
+      
       diff_genes <- differentiallyExpressed()$`Gene Symbol`
       gene_idx <- c(1:gene_cnt, (length(diff_genes)-gene_cnt+1):length(diff_genes))
-
+      
       dg_mat <- c()
       for ( n in gene_idx ) {
         gene_idx <- which(genes$Symbol == diff_genes[n])
-        dat1 <- log2(expression[gene_idx, selected_vector1()] + 0.1)
-        dat2 <- log2(expression[gene_idx, selected_vector2()] + 0.1)
-
-        dg_mat <- rbind(dg_mat,
+        dat1 <- log2(expression[gene_idx, rValues$selected_vector1] + 0.1)
+        dat2 <- log2(expression[gene_idx, rValues$selected_vector2] + 0.1)
+        
+        dg_mat <- rbind(dg_mat, 
                         data.frame(gene = diff_genes[n],
                                    expr = c(dat1, dat2),
                                    group = c(rep("1", nbr_group1),
@@ -134,20 +136,20 @@ shinyServer(function(input, output, session) {
                         )
         )
       }
-
+      
       # dg_mat %>% mutate(
       #   gene = as.factor(gene),
       #   expr = as.numeric(expr),
       #   group = as.factor(group),
       #   panel = as.factor(panel)) %>%
-      dg_mat <-
+      dg_mat <- 
         dg_mat %>% mutate(
           gene = as.character(gene),
           expr = as.numeric(expr),
           group = as.factor(group),
           panel = as.factor(panel)) %>%
         arrange(panel)
-
+      
       dg_mat$gene <- factor(dg_mat$gene, levels = dg_mat$gene)
       ggplot(dg_mat, aes(x=group, y=expr, fill=group)) +
         geom_boxplot() +
@@ -158,56 +160,58 @@ shinyServer(function(input, output, session) {
       plotly_empty()
     }
   })
-
+  
   #output$newPlot <- renderPlotly({
    # input$pop_selected
     #new_tsne <- isolate(selected_data())
     #plot_ly(new_tsne, x = ~x, y = ~y, text = ~key) %>%
      # layout(dragmode = "select")})
 
-
-  selected_vector1 = reactive(
-    {input$pop_one_selected
+  observe({
+    if(input$pop_one_selected==1){
       isolate({
         tsneSubset = tsne[tsne$tSNE_1 %in% selected_data()$x & tsne$tSNE_2 %in% selected_data()$y,]
-        barcodes$Barcode %in% tsneSubset$barcode
-      })})
-
-  selected_vector2 = reactive(
-    {
-      hide('div_select_two')
-      if(input$pop_two_selected == 1){
-        isolate({
-          if(!is.null(selected_data_two())){
-            tsneSubset = tsne[tsne$tSNE_1 %in% selected_data_two()$x & tsne$tSNE_2 %in% selected_data_two()$y,]
-            barcodes$Barcode %in% tsneSubset$barcode
-          } else {
-            # if nothing is selected, select the negative set based on tsne
-            barcodes$Barcode %in% tsne$barcode[!tsne$barcode %in% barcodes$Barcode[selected_vector1()]]
-          }
-        })
-      }
+        rValues$selected_vector1 = barcodes$Barcode %in% tsneSubset$barcode
+      })
     }
-  )
+  })
+  
+  observe({
+    hide('div_select_two')
+    if(input$pop_two_selected == 1){
+      isolate({
+        if(!is.null(selected_data_two())){
+          tsneSubset = tsne[tsne$tSNE_1 %in% selected_data_two()$x & tsne$tSNE_2 %in% selected_data_two()$y,]
+          out = barcodes$Barcode %in% tsneSubset$barcode
+        } else {
+          # if nothing is selected, select the negative set based on tsne
+          out = barcodes$Barcode %in% tsne$barcode[!tsne$barcode %in% barcodes$Barcode[rValues$selected_vector1]]
+        }
+        rValues$selected_vector2 = out
+      })
+    }
+  })
+
+
   second_clicked <-reactive({input$pop_two_selected})
+  
 
-
-
+  
   output$tSNE_summary <- renderPlotly({
     second_clicked()
-    g1 = tsne[ tsne$barcode %in% barcodes$Barcode[isolate({selected_vector1()})],]
-    g2 = tsne[tsne$barcode %in% barcodes$Barcode[isolate({selected_vector2()})],]
+    g1 = tsne[ tsne$barcode %in% barcodes$Barcode[isolate({rValues$selected_vector1})],]
+    g2 = tsne[tsne$barcode %in% barcodes$Barcode[isolate({rValues$selected_vector2})],]
     g1["group"] <- rep('group 1', dim(g1)[1])
     g2["group"] <- rep('group 2', dim(g2)[1])
     both_groups = rbind(g1, g2)
-    plot_ly(both_groups, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~group, colors = c("dark blue", "dark red"),
+    plot_ly(both_groups, x = ~tSNE_1, y = ~tSNE_2, text = ~barcode, color = ~group, colors = c("dark blue", "dark red"), 
             key = ~barcode, source = "selection_plot_two") %>%
                layout(dragmode = "select",xaxis = list(range = c(-40,40)),
                       yaxis = list(range = c(-40,40)))
-
+        
     })
-
-
+    
+  
 
   output$downloadDifGenes = downloadHandler(
     filename = 'difGenes.tsv',
@@ -219,10 +223,10 @@ shinyServer(function(input, output, session) {
   differentiallyExpressed = reactive({
     print('should I calculate dif genes?')
       print('yeah I guess')
-      if(!is.null(selected_vector2()) & !is.null(selected_vector1())){
+      if(!is.null(rValues$selected_vector2) & !is.null(rValues$selected_vector1)){
         show('downloadDifGenes')
-        difGenes(group1 = isolate(selected_vector1()),
-                 group2 = selected_vector2())
+        difGenes(group1 = isolate(rValues$selected_vector1),
+                 group2 = rValues$selected_vector2)
       }
   })
 
@@ -255,8 +259,7 @@ shinyServer(function(input, output, session) {
     NumCells<-table(tsne$id)
     NumCells<-as.data.frame(NumCells)
     plot_ly(NumCells, x=~Var1, y=~Freq, color=~Var1, type='bar') %>%
-      layout(xaxis = ax,
-             yaxis = list(title = "Number of cells"))
+      layout(xaxis = ax)
   })
 
   # plotting selected genes ----------
